51a35fb5fbd80a6bde1addc3bacda143c66c522a
amikulka55 <121448478+amikulka55@users.noreply.github.com>
2023-05-16 20:37:26 +0200

    first commit

---
+++ src/main/java/pl/sda/travelagencyservice/TravelAgencyServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.travelagencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyServiceApplication.class, args);
+	}
+
+}
