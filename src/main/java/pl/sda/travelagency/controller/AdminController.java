7a75addda59ddfa8f031ded6da1f37bff7f5f817
Ewa <ekowalik889@gmail.com>
2023-06-17 12:01:57 +0200

    image

--- src/main/java/pl/sda/travelagency/controller/AdminController.java
+++ src/main/java/pl/sda/travelagency/controller/AdminController.java
@@ -1,18 +1,33 @@
 package pl.sda.travelagency.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.travelagency.dto.TripDto;
+import pl.sda.travelagency.dto.TripOrderDto;
+import pl.sda.travelagency.repository.TripOrderRepository;
+import pl.sda.travelagency.service.TripOrderService;
 
 import java.util.List;
-
+@AllArgsConstructor
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    //public String adminViev(Model model) {
+
+public final TripOrderService tripOrderService;
+public final TripOrderRepository tripOrderRepository;
+@GetMapping
+    public String tripsForAdmin(Model model){
+    List <TripOrderDto> allTrips = tripOrderService.getOrderedTrips();
+
+    model.addAttribute("allTrips", allTrips);
+    return "admin";
+
+
+}
 
 
  /*   @GetMapping
@@ -25,5 +40,5 @@ public class AdminController {
         model.addAttribute("promotingTrips", promotingTrips);
         return "trips";*/
 
-
 }
+
--- src/main/java/pl/sda/travelagency/dto/TripOrderDto.java
+++ src/main/java/pl/sda/travelagency/dto/TripOrderDto.java
@@ -1,15 +1,17 @@
 package pl.sda.travelagency.dto;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Data
+@RequiredArgsConstructor
 public class TripOrderDto {
     public List<SimpleParticipantDto> simpleParticipantDtos;
     String buyerFirstName;
     String buyerLastName;
     String buyerPhone;
     String buyerEmail;
-    double cost;
+    double sum;
 }
