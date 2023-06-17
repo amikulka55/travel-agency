e5bfa1feb36b3e9068fff315579129683721ed0a
amikulka55 <121448478+amikulka55@users.noreply.github.com>
2023-06-17 10:56:49 +0200

    admin controller

--- src/main/java/pl/sda/travelagency/service/CityService.java
+++ src/main/java/pl/sda/travelagency/service/CityService.java
@@ -1,4 +1,54 @@
 package pl.sda.travelagency.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class CityService {
+
+    private final CityRepository cityRepository;
+
+    private final CountryRepository countryRepository;
+
+    private final TravelRepository travelRepository;
+
+    public CityService(CityRepository cityRepository, CountryRepository countryRepository, TravelRepository travelRepository) {
+        this.cityRepository = cityRepository;
+        this.countryRepository = countryRepository;
+        this.travelRepository = travelRepository;
+    }
+
+    public List<CityDto> getCities() {
+        List<CityDto> collect = cityRepository.findAll()
+                .stream()
+                .map(it -> new CityDto(it.getId(), it.getName(), it.getCountry().getId(), it.getCountry().getName()))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    public void addCity(CityDto cityDto) {
+        City city = new City();
+        city.setName(cityDto.getName());
+        Optional<Country> byId = countryRepository.findById(cityDto.getCountryId());
+        byId.ifPresent(it -> {
+            city.setCountry(byId.get());
+            cityRepository.save(city);
+        });
+    }
+    public void deleteById(long id){
+        cityRepository.deleteById(id);
+    }
+
+    public boolean checkCityCanBeDelete(long id) {
+        List<Travel> byFromCityOrToCity = travelRepository.findByFromCityOrToCity(id);
+        if (byFromCityOrToCity.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
--- src/main/java/pl/sda/travelagency/service/CountryService.java
+++ src/main/java/pl/sda/travelagency/service/CountryService.java
@@ -1,4 +1,42 @@
 package pl.sda.travelagency.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class CountryService {
+    private final CountryRepository countryRepository;
+
+    private final ContinentRepository continentRepository;
+
+    public CountryService(CountryRepository countryRepository, ContinentRepository continentRepository) {
+        this.countryRepository = countryRepository;
+        this.continentRepository = continentRepository;
+    }
+
+    public List<CountryDto> getCountries() {
+        List<Country> countries = countryRepository.findAll();
+        List<CountryDto> collect = countries.stream()
+                .map(it -> new CountryDto(it.getId(), it.getName(), it.getContinent().getId(), it.getContinent().getName()))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    public void addCountry(CountryDto countryDto) {
+        Country country = new Country();
+        country.setName(countryDto.getName());
+        Optional<Continent> byId = continentRepository.findById(countryDto.getContinentId());
+        byId.ifPresent(it -> {
+            country.setContinent(byId.get());
+            countryRepository.save(country);
+        });
+    }
+
+    public void deleteById(long id){
+        countryRepository.deleteById(id);
+    }
 }
+
