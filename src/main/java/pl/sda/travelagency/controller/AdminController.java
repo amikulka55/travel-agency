0a4a8b37656adf257ced734002f32c4b137c857a
Ewa <ekowalik889@gmail.com>
2023-06-17 12:31:03 +0200

    image

--- src/main/java/pl/sda/travelagency/controller/AdminController.java
+++ src/main/java/pl/sda/travelagency/controller/AdminController.java
@@ -1,32 +1,27 @@
 package pl.sda.travelagency.controller;
 
-<<<<<<< HEAD
+
 import lombok.AllArgsConstructor;
-=======
->>>>>>> origin/master
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-import pl.sda.travelagency.dto.TripDto;
-=======
->>>>>>> origin/master
+
+
 import pl.sda.travelagency.dto.TripOrderDto;
 import pl.sda.travelagency.repository.TripOrderRepository;
 import pl.sda.travelagency.service.TripOrderService;
 
 import java.util.List;
-<<<<<<< HEAD
-@AllArgsConstructor
-=======
 
->>>>>>> origin/master
+@AllArgsConstructor
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-<<<<<<< HEAD
+
 
 public final TripOrderService tripOrderService;
 public final TripOrderRepository tripOrderRepository;
@@ -40,32 +35,4 @@ public final TripOrderRepository tripOrderRepository;
 
 }
 
-
- /*   @GetMapping
-    public String showTrips(Model model) {
-        List<TripDto> trips = tripService.getTrips();
-        List<TripDto> promotingTrips = trips.stream().filter(TripDto::isPromoted).toList();
-        trips = trips.stream().limit(3).toList();
-
-        model.addAttribute("trips", trips);
-        model.addAttribute("promotingTrips", promotingTrips);
-        return "trips";*/
-
 }
-
-=======
-    private TripOrderService tripOrderService;
-
-
-    @GetMapping
-    public String showTrips(Model model) {
-        List<TripOrderDto> trips = tripOrderService.showTrips();
-       // List<TripOrderDto>
-
-
-    }
-
-    ;
-
-}
->>>>>>> origin/master
--- src/main/java/pl/sda/travelagency/service/TripOrderService.java
+++ src/main/java/pl/sda/travelagency/service/TripOrderService.java
@@ -3,21 +3,37 @@ package pl.sda.travelagency.service;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.sda.travelagency.dto.TripDto;
 import pl.sda.travelagency.dto.TripOrderDto;
+import pl.sda.travelagency.entity.Trip;
 import pl.sda.travelagency.entity.TripOrder;
 import pl.sda.travelagency.repository.TripOrderRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TripOrderService {
 
     private final TripOrderRepository tripOrderRepository;
-    public List<TripOrderDto> showTrips() {
-        List<TripOrder> all = tripOrderRepository.findAll();
-        return null;
+    public List<TripOrderDto> getOrderedTrips(){
+        List<TripOrder> all = tripOrderRepository.tripsFromDB();
+        return all.stream()
+                .map(it-> {
+                    TripOrderDto tripOrderDto = new TripOrderDto();
+                    tripOrderDto.getSimpleParticipantDtos();
+                    tripOrderDto.getBuyerPhone();
+                    tripOrderDto.getBuyerEmail();
+                    tripOrderDto.getBuyerFirstName();
+                    tripOrderDto.getBuyerLastName();
+                    tripOrderDto.getSum();
+                    return tripOrderDto;
+                })
+                .limit(10)
+                .collect(Collectors.toList());
     }
+
 
 
 }
